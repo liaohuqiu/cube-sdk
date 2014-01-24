44ae537b4ffb3f128afd802f6dc6785c6e42b97a
liaohuqiu <liaohuqiu@gmail.com>
2014-01-24 17:19:56 +0800

    hope everything will be well

--- sample-and-tests/src/com/srain/cube/sample/app/CubeApplication.java
+++ sample-and-tests/src/com/srain/cube/sample/app/CubeApplication.java
@@ -5,11 +5,6 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-import com.srain.cube.file.FileUtil;
 import com.srain.cube.image.imple.LruImageFileCache;
 import com.srain.cube.util.LocalDisplay;
 import com.srain.cube.util.SystemWather;
@@ -34,15 +29,7 @@ public class CubeApplication extends Application {
 	}
 
 	public static void initImageLoader(Context context) {
-		// This configuration tuning is custom. You can tune every option, you may tune some of them,
-		// or you can create default configuration by
-		// ImageLoaderConfiguration.createDefault(this);
-		// method.
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context).threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
-				.discCacheFileNameGenerator(new Md5FileNameGenerator()).tasksProcessingOrder(QueueProcessingType.LIFO).writeDebugLogs() // Remove for release app
-				.build();
-		// Initialize ImageLoader with configuration.
-		ImageLoader.getInstance().init(config);
+
 	}
 
 	@Override
