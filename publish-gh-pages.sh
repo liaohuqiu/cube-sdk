2a5d2bfa383b343a9b270205ab55e37d319bcd58
liaohuqiu <liaohuqiu@gmail.com>
2014-02-20 11:33:54 +0800

    update publish script

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+function exe_cmd() {
+    echo $1
+    eval $1
+}
+
 branch=$1
 if [ -z "$branch" ] || [ -a "$branch" == " " ]; then
     branch='gh-pages'
 fi
 
-checkout "$branch"
-cp -r _site/* . && rm -rf _site/ && touch .nojekyll
+jekyll build
+if [ ! -d '_site' ];then
+    echo "not content to be published"
+    exit
+fi
+
+exe_cmd "git checkout $branch"
+error_code=$?
+if [ $error_code != 0 ];then
+    echo 'Switch branch fail.'
+    exit
+else
+    cmd="ls | grep -v _site|xargs rm && cp -r _site/* . && rm -rf _site/ && touch .nojekyll"
+    echo $cmd
+fi
