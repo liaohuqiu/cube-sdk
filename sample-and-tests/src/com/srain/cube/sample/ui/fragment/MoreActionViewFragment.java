bf4befcd3db037cc8b217dc28e5a52aa2dbdc319
liaohuqiu <liaohuqiu@gmail.com>
2014-03-13 16:30:54 +0800

    add more action view

---
+++ sample-and-tests/src/com/srain/cube/sample/ui/fragment/MoreActionViewFragment.java
@@ -0,0 +1,22 @@
+package com.srain.cube.sample.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.srain.cube.sample.R;
+import com.srain.cube.sample.activity.TitleBaseFragment;
+
+public class MoreActionViewFragment extends TitleBaseFragment {
+
+	@Override
+	protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		setHeaderTitle("More Action View Demo");
+
+		mTitleHeaderBar.showMoreMenu();
+
+		View view = inflater.inflate(R.layout.fragment_more_action_view, null);
+		return view;
+	}
+}
