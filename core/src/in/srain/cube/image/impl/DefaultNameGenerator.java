d6575987e26a9352b8a054fe76480f48643284a4
liaohuqiu <liaohuqiu@gmail.com>
2015-04-14 11:17:43 +0800

    update for ImageLoader

---
+++ core/src/in/srain/cube/image/impl/DefaultNameGenerator.java
@@ -0,0 +1,22 @@
+package in.srain.cube.image.impl;
+
+import in.srain.cube.image.ImageLoadRequest;
+import in.srain.cube.image.ImageTask;
+import in.srain.cube.image.iface.NameGenerator;
+
+public class DefaultNameGenerator implements NameGenerator {
+
+    private static DefaultNameGenerator sInstance;
+
+    public static synchronized DefaultNameGenerator getInstance() {
+        if (sInstance == null) {
+            sInstance = new DefaultNameGenerator();
+        }
+        return sInstance;
+    }
+
+    @Override
+    public String generateIdentityUrlFor(ImageLoadRequest request) {
+        return request.getUrl();
+    }
+}
